507b20874d1a938732c2489ebc32829f7fdb09f2
Edward Zhang <charmander@Edwards-MacBook-Pro-2.local>
2014-11-27 16:28:53 -0800

    Initial Commit

---
+++ blitz/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  blitz
+//
+//  Created by Edward Zhang on 11/27/14.
+//  Copyright (c) 2014 Edward Zhang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
